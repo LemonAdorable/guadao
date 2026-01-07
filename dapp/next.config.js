127a3123d6a4968057ab888d10f922da505e269d
IceLing <lemonadorable@gmail.com>
2026-01-07 21:38:27 +0800

    safe support

--- dapp/next.config.js
+++ dapp/next.config.js
@@ -5,6 +5,25 @@ const nextConfig = {
     config.resolve.alias["@react-native-async-storage/async-storage"] = false;
     return config;
   },
+  async headers() {
+    return [
+      {
+        source: '/manifest.json',
+        headers: [
+          { key: 'Access-Control-Allow-Origin', value: '*' },
+          { key: 'Access-Control-Allow-Methods', value: 'GET' },
+          { key: 'Access-Control-Allow-Headers', value: 'X-Requested-With, content-type, Authorization' },
+        ],
+      },
+      {
+        source: '/:path*',
+        headers: [
+          { key: 'Access-Control-Allow-Origin', value: '*' }, // Allow Safe to fetch resources
+          // Removing X-Frame-Options to allow framing
+        ],
+      }
+    ];
+  },
 };
 
 module.exports = nextConfig;
